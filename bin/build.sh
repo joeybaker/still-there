4069572c00506ac45e925c548098e084ec844db9
Joey Baker <joey@byjoeybaker.com>
2018-05-17 14:41:39 -0700

    Add build script

---
+++ bin/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# strict mode http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\n\t'
+
+command -v yarn >/dev/null 2>&1 || { npm i -g yarn@1.6; }
+yarn install
+yarn build
+docker build -t joeybaker/still-there .
+
